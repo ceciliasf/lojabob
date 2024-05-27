19ba30c23324ed0f37e2331f69ec1d811af797ff
Cecília Santiago <125615984+ceciliasf@users.noreply.github.com>
2024-05-27 18:10:13 -0300

    .
    .

--- teste.js
+++ teste.js
@@ -1,3 +1,7 @@
 const app = require('./app2')
 
 app.inserir('pepsi','pepsilight','pepsi@gmail.com')
+
+//trocar dados, terminal, node teste, bd.sql, selecione use bob botao direito do mouse,seleciona runmysqlquery refresh
+
+app.apagar(2)
